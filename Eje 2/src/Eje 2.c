7874d5c5180019dcb2a889e2668f6d919fe48c94
Carlos Gauto <rca.gauto@gmail.com>
2021-10-18 20:07:21 -0300

    Ej. Informes

--- Eje 2/src/Eje 2.c
+++ Eje 2/src/Eje 2.c
@@ -11,7 +11,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int swap(int* num1, int* num2);
+
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	int a = 5;
+	int b = 10;
+
+	printf("a = %d ----- b = %d\n", a, b);
+
+	swap(&a,&b);
+	printf("a = %d ----- b = %d\n", a, b);
+
+
 	return EXIT_SUCCESS;
+}
+int swap(int* num1, int* num2){
+	int aux;
+	int retorno;
+
+	aux = *num1;
+	*num1 = *num2;
+	*num2 = aux;
+
+	return retorno;
 }
